e64f76c7ba4c9e55ad352da4ab6c3c7dfa67dfd0
Phoenix He <tih@microsoft.com>
2019-09-10 18:36:42 +0800

    Generated from 81fb55e8252598b44b1d5ee4db527eacba725546
    Fix Local Search Swagger

--- services/cognitiveservices/v1.0/localsearch/client.go
+++ services/cognitiveservices/v1.0/localsearch/client.go
@@ -23,29 +23,29 @@ package localsearch
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"github.com/Azure/go-autorest/autorest"
+    "github.com/Azure/go-autorest/autorest"
 )
 
 const (
-	// DefaultBaseURI is the default URI used for the service Localsearch
-	DefaultBaseURI = "https://api.cognitive.microsoft.com/localbusinesses/v7.0/search"
-)
+// DefaultBaseURI is the default URI used for the service Localsearch
+DefaultBaseURI = "https://api.cognitive.microsoft.com/bing")
 
 // BaseClient is the base client for Localsearch.
 type BaseClient struct {
-	autorest.Client
-	BaseURI string
+    autorest.Client
+    BaseURI string
 }
 
 // New creates an instance of the BaseClient client.
-func New() BaseClient {
-	return NewWithBaseURI(DefaultBaseURI)
+func New()BaseClient {
+    return NewWithBaseURI(DefaultBaseURI, )
 }
 
 // NewWithBaseURI creates an instance of the BaseClient client.
-func NewWithBaseURI(baseURI string) BaseClient {
-	return BaseClient{
-		Client:  autorest.NewClientWithUserAgent(UserAgent()),
-		BaseURI: baseURI,
-	}
+func NewWithBaseURI(baseURI string, ) BaseClient {
+    return BaseClient{
+        Client: autorest.NewClientWithUserAgent(UserAgent()),
+        BaseURI: baseURI,
+    }
 }
+
--- services/cognitiveservices/v1.0/localsearch/localsearchapi/interfaces.go
+++ services/cognitiveservices/v1.0/localsearch/localsearchapi/interfaces.go
@@ -18,13 +18,13 @@ package localsearchapi
 // Changes may cause incorrect behavior and will be lost if the code is regenerated.
 
 import (
-	"context"
-	"github.com/Azure/azure-sdk-for-go/services/cognitiveservices/v1.0/localsearch"
+    "context"
+    "github.com/Azure/azure-sdk-for-go/services/cognitiveservices/v1.0/localsearch"
 )
 
-// LocalClientAPI contains the set of methods on the LocalClient type.
-type LocalClientAPI interface {
-	Search(ctx context.Context, query string, acceptLanguage string, pragma string, userAgent string, clientID string, clientIP string, location string, countryCode string, market string, localCategories string, localCircularView string, localMapView string, count string, first string, responseFormat []localsearch.ResponseFormat, safeSearch localsearch.SafeSearch, setLang string) (result localsearch.SearchResponse, err error)
-}
+        // LocalClientAPI contains the set of methods on the LocalClient type.
+        type LocalClientAPI interface {
+            Search(ctx context.Context, query string, acceptLanguage string, pragma string, userAgent string, clientID string, clientIP string, location string, countryCode string, market string, localCategories string, localCircularView string, localMapView string, count string, first string, responseFormat []localsearch.ResponseFormat, safeSearch localsearch.SafeSearch, setLang string) (result localsearch.SearchResponse, err error)
+        }
 
-var _ LocalClientAPI = (*localsearch.LocalClient)(nil)
+        var _ LocalClientAPI = (*localsearch.LocalClient)(nil)
