02ae65bb9ba9a8a4cfe717afd9473f1f60f6e0c6
SDK Automation <sdkautomation@microsoft.com>
2019-10-21 23:05:32 +0000

    Generated from 1ad3f52e1acc6d2389f63ccddb941e15a3af9377
    update to avoid breaking changes.

--- services/resources/mgmt/2015-12-01/features/version.go
+++ services/resources/mgmt/2015-12-01/features/version.go
@@ -19,12 +19,12 @@ package features
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/1.0.0 features/2015-12-01"
+	return "Azure-SDK-For-Go/1.0.1 features/2015-12-01"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-	return "1.0.0"
+	return "1.0.1"
 }
 
-// tag: services/resources/mgmt/2015-12-01/features/v1.0.0
+// tag: services/resources/mgmt/2015-12-01/features/v1.0.1
