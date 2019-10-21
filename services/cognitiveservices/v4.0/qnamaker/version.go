2eee13fa676d86a0ce33c985bf3487581963f052
SDK Automation <sdkautomation@microsoft.com>
2019-10-21 10:37:27 +0000

    Generated from 53f49a5402c416fa6143aefc0c541491bbcad1ff
    PR comments

--- services/cognitiveservices/v4.0/qnamaker/version.go
+++ services/cognitiveservices/v4.0/qnamaker/version.go
@@ -19,7 +19,7 @@ package qnamaker
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/1.0.0 qnamaker/v4.0"
+	return "Azure-SDK-For-Go/1.0.0 qnamaker/4.0"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
