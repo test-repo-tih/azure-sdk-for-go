852b78fa7b838a492700dffa797d088ec84d8323
SDK Automation <sdkautomation@microsoft.com>
2019-10-21 18:56:13 +0000

    Generated from cd3aa7384320ba0e3ed6d30bbadb199e2b1c9f45
    Merge branch 'network-october-release' into network-patch
    
    # Conflicts:
    #	specification/network/resource-manager/Microsoft.Network/stable/2019-09-01/routeFilter.json

--- services/network/mgmt/2018-08-01/network/version.go
+++ services/network/mgmt/2018-08-01/network/version.go
@@ -19,12 +19,12 @@ package network
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/1.0.0 network/2018-08-01"
+	return "Azure-SDK-For-Go/1.0.1 network/2018-08-01"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-	return "1.0.0"
+	return "1.0.1"
 }
 
-// tag: services/network/mgmt/2018-08-01/network/v1.0.0
+// tag: services/network/mgmt/2018-08-01/network/v1.0.1
