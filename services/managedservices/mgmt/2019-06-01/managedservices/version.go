ae5c5c1fa3abd760d00cf1aa199cd58683015584
SDK Automation <sdkautomation@microsoft.com>
2019-10-22 00:59:32 +0000

    Generated from 458d27229157e306c716253a135b8189549fd66c
    Using the filter for collection call to return specific definitions.
    Created separate path for point get call.
    
    Previous comment: New api-version, having new resource type - marketplaceregistrationdefinition.

--- services/managedservices/mgmt/2019-06-01/managedservices/version.go
+++ services/managedservices/mgmt/2019-06-01/managedservices/version.go
@@ -19,12 +19,12 @@ package managedservices
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/1.0.0 managedservices/2019-06-01"
+	return "Azure-SDK-For-Go/1.0.1 managedservices/2019-06-01"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-	return "1.0.0"
+	return "1.0.1"
 }
 
-// tag: services/managedservices/mgmt/2019-06-01/managedservices/v1.0.0
+// tag: services/managedservices/mgmt/2019-06-01/managedservices/v1.0.1
