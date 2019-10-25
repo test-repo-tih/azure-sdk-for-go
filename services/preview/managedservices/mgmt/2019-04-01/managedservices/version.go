5485f27b4c2fa44dd29e2181ce94691d13074dcf
SDK Automation <sdkautomation@microsoft.com>
2019-10-25 10:23:59 +0000

    Generated from a71df15a587ec060147aaef2dcfc12d19500fdc8
    Fix json format by prettier

--- services/preview/managedservices/mgmt/2019-04-01/managedservices/version.go
+++ services/preview/managedservices/mgmt/2019-04-01/managedservices/version.go
@@ -19,7 +19,7 @@ package managedservices
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/1.0.0 managedservices/2019-04-01"
+	return "Azure-SDK-For-Go/1.0.0 managedservices/2019-04-01-preview"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
