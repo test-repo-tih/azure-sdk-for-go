4222daeae69516821bb5d2b54e8354ae163f60cb
SDK Automation <sdkautomation@microsoft.com>
2019-10-25 01:07:04 +0000

    Generated from 5fb9c7773380b4166f03bd469ad62a086494777e
    Add PGSQL to AzurePGSQL swagger changes

--- services/preview/datamigration/mgmt/2018-03-31-preview/datamigration/version.go
+++ services/preview/datamigration/mgmt/2018-03-31-preview/datamigration/version.go
@@ -19,12 +19,12 @@ package datamigration
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/1.0.0 datamigration/2018-03-31-preview"
+	return "Azure-SDK-For-Go/1.0.1 datamigration/2018-03-31-preview"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
 func Version() string {
-	return "1.0.0"
+	return "1.0.1"
 }
 
-// tag: services/preview/datamigration/mgmt/2018-03-31-preview/datamigration/v1.0.0
+// tag: services/preview/datamigration/mgmt/2018-03-31-preview/datamigration/v1.0.1
